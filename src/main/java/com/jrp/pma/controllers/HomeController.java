1bb532f226bd6af713ab3191de8fadbb30230f6c
kmarabet <kamilmarabet@gmail.com>
2020-02-28 18:14:10 +0300

    32. Property Configurations and Reading Values

--- src/main/java/com/jrp/pma/controllers/HomeController.java
+++ src/main/java/com/jrp/pma/controllers/HomeController.java
@@ -9,6 +9,7 @@ import com.jrp.pma.dto.ChartData;
 import com.jrp.pma.dto.EmployeeProject;
 import com.jrp.pma.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,9 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    @Value("${version}")
+    private String var;
+
     @Autowired
     ProjectRepository proRepo;
     @Autowired
@@ -26,6 +30,8 @@ public class HomeController {
 
     @GetMapping("/")
     public String displayHome(Model model) throws JsonProcessingException {
+
+        model.addAttribute("versionNumber", var);
 
         Map<String, Object> map = new HashMap<>();
 
