0ed86b18991c751e9a1bb6272575dae9c6c39239
kmarabet <kamilmarabet@gmail.com>
2020-03-04 17:28:34 +0300

    37. Using @SpringBootTest Annotation Correctly

--- src/test/java/com/jrp/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/jrp/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,22 +1,19 @@
-package com.jrp.dao;
+package com.jrp.pma.dao;
 
-import com.jrp.pma.ProjectManagementApplication;
-import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.entities.Project;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@ContextConfiguration(classes={ProjectManagementApplication.class})
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
         @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = {"classpath:drop.sql"}) })
 public class ProjectRepositoryIntegrationTest {
