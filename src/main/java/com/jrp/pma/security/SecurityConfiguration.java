fe4cec22b39b7cd6f66dcf3f9d1c828e5701763c
kmarabet <kamilmarabet@gmail.com>
2020-03-12 18:51:46 +0300

    51. Spring Security: JDBC Backed Security

--- src/main/java/com/jrp/pma/security/SecurityConfiguration.java
+++ src/main/java/com/jrp/pma/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.jrp.pma.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,14 +10,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
+
+//        auth.inMemoryAuthentication()
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .withDefaultSchema()
                 .withUser("myuser")
                 .password("pass")
                 .roles("USER")
@@ -41,6 +49,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/projects/new").hasRole("ADMIN")
                 .antMatchers("/employees/new").hasRole("ADMIN")
+                .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/").authenticated().and().formLogin();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 }
