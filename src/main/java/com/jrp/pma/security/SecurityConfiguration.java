8ec1854bf7d24389e380e1296495daffb618d2fe
kmarabet <kamilmarabet@gmail.com>
2020-03-12 17:17:07 +0300

    49-50. Spring Security: Basic In-Memory Authentication (with users) and Authorization with roles.

---
+++ src/main/java/com/jrp/pma/security/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.jrp.pma.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("myuser")
+                .password("pass")
+                .roles("USER")
+        .and()
+                .withUser("taz")
+                .password("pass2")
+                .roles("USER")
+        .and()
+                .withUser("managerUser")
+                .password("pass3")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/projects/new").hasRole("ADMIN")
+                .antMatchers("/employees/new").hasRole("ADMIN")
+                .antMatchers("/").authenticated().and().formLogin();
+    }
+}
